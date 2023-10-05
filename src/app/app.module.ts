c7981a151529649f92f59eb30b924876a92f5e8d
eliaquimDev <eliaquimdg@gmail.com>
2023-10-05 10:52:48 -0300

    componente header criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,9 +29,10 @@ import { MatCardModule } from "@angular/material/card";
 //Componentes do projeto
 import { NavComponent } from "./components/nav/nav.component";
 import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, NavComponent, HomeComponent],
+  declarations: [AppComponent, NavComponent, HomeComponent, HeaderComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
